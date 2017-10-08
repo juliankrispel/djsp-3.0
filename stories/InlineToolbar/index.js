9efcf53b94165f432c02b4d338587c85cac044b5
Julian Krispel <julian@reactrocket.com>
2017-10-08 14:39:53 +0100

    start inline toolbar story

---
+++ stories/InlineToolbar/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { EditorState } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import Popover from 'djsp.popover';
+
+export default () => (editor);
